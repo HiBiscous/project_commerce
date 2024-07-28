2e48a144d807c343b324172c9d1d26adcda36870
HiBiscous <hiba.babouri@gmail.com>
2024-07-28 18:14:08 +0200

    feat(trait): add enableTrait

--- src/Entity/Gender.php
+++ src/Entity/Gender.php
@@ -3,12 +3,14 @@
 namespace App\Entity;
 
 use App\Repository\GenderRepository;
+use App\Traits\EnableTrait;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: GenderRepository::class)]
 class Gender
 {
+    use EnableTrait;
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -18,10 +20,6 @@ class Gender
     #[Assert\Length(max: 255, maxMessage: 'Le nom ne doit pas dépasser 255 caractères')]
     #[Assert\NotBlank()]
     private ?string $name = null;
-
-    #[ORM\Column]
-    #[Assert\NotBlank()]
-    private ?bool $enable = null;
 
     public function getId(): ?int
     {
@@ -36,18 +34,6 @@ class Gender
     public function setName(string $name): static
     {
         $this->name = $name;
-
-        return $this;
-    }
-
-    public function isEnable(): ?bool
-    {
-        return $this->enable;
-    }
-
-    public function setEnable(bool $enable): static
-    {
-        $this->enable = $enable;
 
         return $this;
     }
