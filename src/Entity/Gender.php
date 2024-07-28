0529f866dc6ef370aa011b29de56a4f57418dc55
HiBiscous <hiba.babouri@gmail.com>
2024-07-28 17:41:46 +0200

    feat(gender): done CRUD gender0

---
+++ src/Entity/Gender.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GenderRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: GenderRepository::class)]
+class Gender
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\Length(max: 255, maxMessage: 'Le nom ne doit pas dépasser 255 caractères')]
+    #[Assert\NotBlank()]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    #[Assert\NotBlank()]
+    private ?bool $enable = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function isEnable(): ?bool
+    {
+        return $this->enable;
+    }
+
+    public function setEnable(bool $enable): static
+    {
+        $this->enable = $enable;
+
+        return $this;
+    }
+}
