3584f5283f1935b498c2f11db0028304e742d05b
HiBiscous <hiba.babouri@gmail.com>
2024-07-29 15:01:36 +0200

    feat(marque): add index and create backend

---
+++ src/Controller/Backend/MarqueController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller\Backend;
+
+use App\Entity\Marque;
+use App\Form\MarqueType;
+use App\Repository\MarqueRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('admin/marque', name: 'admin.marque')]
+class MarqueController extends AbstractController
+{
+    public function __construct(private EntityManagerInterface $em)
+    {
+    }
+
+    #[Route('/', name: '.index', methods: ['GET', 'POST'])]
+    public function index(MarqueRepository $repo): Response
+    {
+        return $this->render('Backend/Marque/index.html.twig', [
+            'marque' => $repo->findAll(),
+        ]);
+    }
+
+    #[Route('/create', name: '.create', methods: ['GET', 'POST'])]
+    public function create(Request $request): Response|RedirectResponse
+    {
+        $marque = new Marque;
+        $form = $this->createForm(MarqueType::class, $marque);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($marque);
+            $this->em->flush();
+
+            $this->addFlash('success', 'La marque a bien été mis à jour');
+
+            return $this->redirectToRoute('admin.marque.index');
+        }
+
+        return $this->render('Backend/Marque/create.html.twig', [
+            'form' => $form
+        ]);
+    }
+}
